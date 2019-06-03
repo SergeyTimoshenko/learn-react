4f9659a95d31069c2e6644236fda037db39ec7c8
Sergey Timoschenko <timoshenko951@gmail.com>
2019-06-03 23:00:38 +0300

    description show/hide

--- src/task/task.modal.jsx
+++ src/task/task.modal.jsx
@@ -6,14 +6,28 @@ import { Assignment, Subject, Comment } from '@material-ui/icons';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class Task extends React.Component {
     constructor(props) {
         super(props)
         console.log(props)
         this.state = {
-            topic: props.topic
+            topic: props.topic,
+            isEditDescription: false,
+            isDisabledSave: true
         }
+    }
+    onEditDescription = () => {
+        this.setState({isEditDescription: true, isDisabledSave: false})
+    }
+    onBlurDescription = () => {
+        setTimeout(() => {
+            this.setState({isEditDescription: false, isDisabledSave: true})
+        }, 200)
+    }
+    onSaveDescription = () => {
+        console.log('yay')
     }
     resetMargin = {
         margin: 0
@@ -24,14 +38,12 @@ class Task extends React.Component {
                     aria-labelledby="simple-modal-title"
                     aria-describedby="simple-modal-description"
                 >
-                
                     <div style={{
                         position:'absolute',
                         top:200,
                         width: 600,
                         left: 'calc(50vw - 300px)',
                     }}>
-                    {console.log(this.props)}
                         <Card style={{
                             padding: 20
                         }}>
@@ -50,13 +62,37 @@ class Task extends React.Component {
                                 <Grid item xs={1}>
                                     <Subject />
                                 </Grid>
-                                <Grid item xs={11}>
+                                <Grid item xs={11} onBlur={this.onBlurDescription}>
                                     <h3 style={this.resetMargin}>Description</h3>
-                                    <Paper className="card-descroption">
-                                        <Typography component="p">
-                                            Some description
-                                        </Typography>
-                                    </Paper>
+                                    {
+                                        this.state.isEditDescription ? (
+                                            <TextField
+                                                autoFocus
+                                                label="Some description"
+                                                multiline
+                                                variant="outlined"
+                                                id="mui-theme-provider-outlined-input"
+                                                style={{marginTop: 20, width: '100%'}}
+                                                
+                                            ></TextField>
+                                        ) : (
+                                            <Paper className="card-descroption" onClick={this.onEditDescription}>
+                                                <Typography component="p">
+                                                    Some description
+                                                </Typography>
+                                            </Paper>
+                                        )
+                                    }
+                                    
+                                    <Button variant="contained" color="primary" style={{
+                                        marginTop:10,
+                                    }}
+                                    disabled={this.state.isDisabledSave}
+                                    onClick={this.onSaveDescription}
+                                    >
+                                        save
+                                    </Button>
+                                    
                                 </Grid>
                             </Grid>
                             <Grid container spacing={8} style={{marginTop: 20}}>
@@ -76,8 +112,7 @@ class Task extends React.Component {
                             </Grid>
                             
                         </Card> 
-                    </div>
-                    
+                    </div> 
                 </Modal>
         )
     }
